0726480067e3e1dfa6f7c80e350b62e8132a6411
oaksoeaung711 <oaksoeaung.dev@gmail.com>
2024-01-14 20:25:50 +0630

    Project Done

--- database/seeders/ContactSeeder.php
+++ database/seeders/ContactSeeder.php
@@ -14,7 +14,7 @@ class ContactSeeder extends Seeder
             "email" => "EDCactivitiescenter@gmail.com",
             "primaryphonenumber" => "09771435596",
             "secondaryphonenumber" => "09771435596",
-            "worktime" => "9 AM 4 PM",
+            "worktime" => "9 AM - 4 PM",
             "address" => "No.(504/506), Room A (4),39\\th,supper\\ St (Lower Block), Kyauktada, Yangon",
             "facebookurl" => "https://www.facebook.com/profile.php?id=100095479575321",
             "googlemapurl" => "https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d2311.6194359644987!2d96.11795676851716!3d16.85711385745237!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2smm!4v1704032545274!5m2!1sen!2smm"
